acb5a2bfa3c8509089542ad9b046286debc0ea51
nikolai-chernolutskii <nikolai@site-now.online>
2023-02-02 16:48:41 +0200

    refactor: single out Results.jsx component

---
+++ src/Results.jsx
@@ -0,0 +1,28 @@
+import Pet from "./Pet";
+
+// Array destructuring. This component will be taking the props from the parent component (SearchParams.jsx) and we expect there to be a prop with the _name_ "pets", so we want to pull out specifically the { pets } part of the props.
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => {
+          return (
+            <Pet
+              animal={pet.animal}
+              key={pet.id}
+              name={pet.name}
+              breed={pet.breed}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
+              id={pet.id}
+            />
+          );
+        })
+      )}
+    </div>
+  );
+};
+
+export default Results;
